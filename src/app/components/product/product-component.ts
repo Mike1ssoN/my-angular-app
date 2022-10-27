724942eaa821befc4d7600aec687ba83a40057a8
Misha <97121455+Mike1ssoN@users.noreply.github.com>
2022-10-27 13:43:01 +0300

    start

---
+++ src/app/components/product/product-component.ts
@@ -0,0 +1,16 @@
+﻿import {Component, Input} from "@angular/core";
+import {IProduct} from "../../models/product";
+
+
+@Component({
+
+  selector:'app-product',
+  templateUrl:'product-component.html'
+})
+export  class  ProductComponent{
+
+  @Input() product:IProduct
+
+}
+
+
